3ec1e5db63021a14a57176f822192a1283458831
MGuiHu <miguelhallegado@gmail.com>
2026-05-21 09:18:25 +0200

    Increase iframe minimum height from 400px to 560px

--- src/pages/Eventoads.tsx
+++ src/pages/Eventoads.tsx
@@ -54,7 +54,7 @@ const Eventoads = () => {
                     frameBorder="0" 
                     scrolling="no" 
                     width="100%" 
-                    className="ipz-iframe min-h-[400px]"
+                    className="ipz-iframe min-h-[560px]"
                     title="Formulario de registro"
                   />
                   <script 
